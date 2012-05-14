5c048e3990559bb6eaab7c2485cf6d833ce15616
Daniel Roethlisberger <daniel@roe.ch>
2012-05-14 21:43:24 +0200

    Remove unneeded include statements

--- base64.t.c
+++ base64.t.c
@@ -28,9 +28,7 @@
 
 #include "base64.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include <string.h>
 
 #include <check.h>
 
--- url.t.c
+++ url.t.c
@@ -28,9 +28,7 @@
 
 #include "url.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include <string.h>
 
 #include <check.h>
 
