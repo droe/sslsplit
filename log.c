06c61c16ed55b98da5ad5989c60f9ccc9dbad4fa
Landon Fuller <landon@landonf.org>
2014-10-18 16:40:22 -0600

    Add support for specifying log paths as a specialized format string.
    Format string handling is fully implemented, with the exception of
    support for automatically creating missing directories.

--- log.c
+++ log.c
@@ -40,6 +40,7 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <syslog.h>
+#include <assert.h>
 
 /*
  * Centralized logging code multiplexing thread access to the logger based
@@ -219,6 +220,7 @@ log_connect_close(void)
 logger_t *content_log = NULL;
 static int content_fd = -1; /* if set, we are in single file mode */
 static const char *content_basedir = NULL;
+static const char *content_logspec = NULL;
 
 static int
 log_content_open_singlefile(const char *logfile)
@@ -239,6 +241,13 @@ log_content_open_logdir(const char *basedir)
 	return 0;
 }
 
+static int
+log_content_open_logspec(const char *logspec)
+{
+	content_logspec = logspec;
+	return 0;
+}
+
 static void
 log_content_close_singlefile(void)
 {
@@ -248,12 +257,125 @@ log_content_close_singlefile(void)
 	}
 }
 
+/*
+ * Generate a log path based on the given log spec.
+ * Returns an allocated buffer which must be freed by caller, or NULL on error.
+ */
+static char *
+log_content_format_pathspec(const char *logspec, char *srcaddr, char *dstaddr,
+			    char *exec_path, char *user, char *group)
+{
+	/* set up buffer to hold our generated file path */
+	size_t path_buflen = 1024;
+	char *path_buf = malloc(path_buflen);
+	if (path_buf == NULL) {
+		log_err_printf("failed to allocate path buffer\n");
+		return NULL;
+	}
+
+	/* iterate over format specifiers */
+	size_t path_len = 0;
+	for (const char *p = logspec; *p != '\0'; p++) {
+		const char *elem = NULL;
+		size_t elem_len = 0;
+
+		const char iso8601[] =  "%Y%m%dT%H%M%SZ";
+		char timebuf[24]; // sized for ISO 8601 format
+
+		/* parse the format string and generate the next path element */
+		switch (*p) {
+			case '%':
+				p++;
+				/* handle format specifiers. */
+				switch (*p) {
+					case '\0':
+						/* unexpected eof; backtrack and discard invalid format spec */
+						p--;
+						elem_len = 0;
+						break;
+					case '%':
+						elem = p;
+						elem_len = 1;
+						break;
+					case 'd':
+						elem = dstaddr;
+						elem_len = strlen(dstaddr);
+						break;
+					case 's':
+						elem = srcaddr;
+						elem_len = strlen(srcaddr);
+						break;
+					case 'x':
+						if (exec_path) {
+							char *match = exec_path;
+							while ((match = strstr(match, "/")) != NULL) {
+								match++;
+								elem = match;
+							}
+							elem_len = strlen(elem);
+						} else {
+							elem_len = 0;
+						}
+						break;
+					case 'X':
+						elem = exec_path;
+						elem_len = exec_path ? strlen(exec_path) : 0;
+						break;
+					case 'u':
+						elem = user;
+						elem_len = user ? strlen(user) : 0;
+						break;
+					case 'g':
+						elem = group;
+						elem_len = group ? strlen(group) : 0;
+						break;
+					case 'T': {
+						time_t epoch;
+						struct tm *utc;
+
+						time(&epoch);
+						utc = gmtime(&epoch);
+						strftime(timebuf, sizeof(timebuf), iso8601, utc);
+
+						elem = timebuf;
+						elem_len = sizeof(timebuf);
+						break;
+					}
+				}
+				break;
+			default:
+				elem = p;
+				elem_len = 1;
+				break;
+		}
+
+		/* growing the buffer to fit elem_len + terminating \0 */
+		if (path_buflen - path_len < elem_len + 1) {
+			/* grow in 1024 chunks. note that the use of `1024' provides our gauranteed space for a trailing '\0' */
+			path_buflen += elem_len + 1024;
+			char *newbuf = realloc(path_buf, path_buflen);
+			if (newbuf == NULL) {
+				log_err_printf("failed to reallocate path buffer");
+				free(path_buf);
+				return NULL;
+			}
+			path_buf = newbuf;
+		}
+
+		strncat(path_buf, elem, elem_len);
+		path_len += elem_len;
+	}
+
+	/* apply terminating NUL */
+	assert(path_buflen > path_len);
+	path_buf[path_len] = '\0';
+	return path_buf;
+}
+
 void
 log_content_open(log_content_ctx_t *ctx, char *srcaddr, char *dstaddr,
 		 char *exec_path, char *user, char *group)
 {
-	log_err_printf("log %s (%s:%s)\n", exec_path, user, group);
-
 	if (ctx->open)
 		return;
 
@@ -261,14 +383,27 @@ log_content_open(log_content_ctx_t *ctx, char *srcaddr, char *dstaddr,
 		ctx->fd = content_fd;
 		asprintf(&ctx->header_in, "%s -> %s", srcaddr, dstaddr);
 		asprintf(&ctx->header_out, "%s -> %s", dstaddr, srcaddr);
-		// TODO - Add something to context based on exec_path / user / group
+	} else if (content_logspec) {
+		char *filename;
+
+		filename = log_content_format_pathspec(content_logspec,
+						       srcaddr, dstaddr,
+						       exec_path, user,
+						       group);
+
+		// TODO - Create directories as required
+
+                ctx->fd = open(filename, O_WRONLY|O_APPEND|O_CREAT, 0660);
+                if (ctx->fd == -1) {
+                        log_err_printf("Failed to open '%s': %s\n",
+                                       filename, strerror(errno));
+                }
+
 	} else {
 		char filename[1024];
 		char timebuf[24];
 		time_t epoch;
 		struct tm *utc;
-
-		// TODO - Open a path based on exec_path / user / group
 
 		time(&epoch);
 		utc = gmtime(&epoch);
@@ -389,12 +524,15 @@ int
 log_preinit(opts_t *opts)
 {
 	if (opts->contentlog) {
-		if (!opts->contentlogdir) {
-			if (log_content_open_singlefile(opts->contentlog)
-			    == -1)
+		if (opts->contentlogdir) {
+			if (log_content_open_logdir(opts->contentlog) == -1)
+				goto out;
+		} else if (opts->contentlogspec) {
+			if (log_content_open_logspec(opts->contentlog) == -1)
 				goto out;
 		} else {
-			if (log_content_open_logdir(opts->contentlog) == -1)
+			if (log_content_open_singlefile(opts->contentlog)
+			    == -1)
 				goto out;
 		}
 		if (!(content_log = logger_new(log_content_writecb))) {
