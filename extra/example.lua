f74a33f8327cc45339defb9dc70eef882520760f
Daniel Roethlisberger <daniel@roe.ch>
2014-12-14 20:20:00 +0100

    Correct the previously incorrect claim in the comment

--- extra/example.lua
+++ extra/example.lua
@@ -1,7 +1,6 @@
 -- Example modify function.  The current implementation does not differentiate
--- between requests and responses and cannot modify the Content-Length HTTP
--- header yet if the HTTP body size changes as a result of the modifications,
--- so the usefulness of this is currently somewhat limited.
+-- between requests and responses and depending on proxyspec type has different
+-- limitations that are subject to change as the feature is being improved.
 
 function modify(data)
   data = string.gsub(data, "gzip, deflate", "") --for requests
