1f1f7b5559f268e5dbcbd885bf479521cdfca08f
Richard Poole <rp@guests.deus.net>
2015-04-18 11:50:26 +0100

    bugfix: correct calls to log_dbg_printf

--- pxyconn.c
+++ pxyconn.c
@@ -556,7 +556,7 @@ pxy_ossl_sessnew_cb(MAYBE_UNUSED SSL *ssl, SSL_SESSION *sess)
 	if (sess) {
 		log_dbg_print_free(ssl_session_to_str(sess));
 	} else {
-		log_dbg_print("(null)\n");
+		log_dbg_printf("(null)\n");
 	}
 #endif /* DEBUG_SESSION_CACHE */
 #ifdef WITH_SSLV2
@@ -587,7 +587,7 @@ pxy_ossl_sessremove_cb(UNUSED SSL_CTX *sslctx, SSL_SESSION *sess)
 	if (sess) {
 		log_dbg_print_free(ssl_session_to_str(sess));
 	} else {
-		log_dbg_print("(null)\n");
+		log_dbg_printf("(null)\n");
 	}
 #endif /* DEBUG_SESSION_CACHE */
 	if (sess) {
