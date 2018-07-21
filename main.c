081b5cf4eed9c78dfb08cbdef598aa52d944f1f8
Daniel Roethlisberger <daniel@roe.ch>
2018-07-21 11:42:07 +0200

    Add newline

--- main.c
+++ main.c
@@ -1006,7 +1006,7 @@ main(int argc, char *argv[])
 		               strerror(errno), errno);
 		exit(EXIT_FAILURE);
 	}
-	log_dbg_printf("Dropped privs to user '%s' group '%s' chroot '%s'",
+	log_dbg_printf("Dropped privs to user '%s' group '%s' chroot '%s'\n",
 	               opts->dropuser, opts->dropgroup, opts->jaildir);
 	if (ssl_reinit() == -1) {
 		fprintf(stderr, "%s: failed to reinit SSL\n", argv0);
