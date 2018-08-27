f5992914dd62a81a454a2266ad225355947d0e9e
Daniel Roethlisberger <daniel@roe.ch>
2018-08-27 20:43:44 +0200

    Add explicit OpenSSL and LibreSSL from source travis targets

---
+++ Mk/bin/install-opt.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+if [ -z "$SSL" ]; then
+	echo '$SSL not set, aborting' >&2
+	exit 1
+fi
+if [ -z "$EVENT" ]; then
+	echo '$EVENT not set, aborting' >&2
+	exit 1
+fi
+
+case "$SSL" in
+openssl-*)
+	SSLURL=https://www.openssl.org/source/$SSL.tar.gz
+	;;
+libressl-*)
+	#SSLURL=https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/$SSL.tar.gz
+	SSLURL=http://ftp.fau.de/pub/OpenBSD/LibreSSL/$SSL.tar.gz
+	;;
+*)
+	exit 1
+	;;
+esac
+
+case "$EVENT" in
+libevent-2.1.8)
+	EVENTURL=https://github.com/libevent/libevent/releases/download/release-2.1.8-stable/libevent-2.1.8-stable.tar.gz
+	EVENTPATCH=Mk/patches/libevent-2.1.8.diff
+	;;
+libevent-2.0.22)
+	EVENTURL=https://github.com/libevent/libevent/releases/download/release-2.0.22-stable/libevent-2.0.22-stable.tar.gz
+	;;
+*)
+	exit 1
+	;;
+esac
+
+if [ ! -d "$HOME/opt/$SSL" ]; then
+	wget "$SSLURL" || exit 1
+	tar -xzvf "$SSL.tar.gz" || exit 1
+	cd "$SSL" || exit 1
+	./config shared --prefix="$HOME/opt/$SSL" || exit 1
+	make && make install || { rm -rf "$HOME/opt/$SSL"; exit 1; }
+	cd ..
+fi
+
+export CPPFLAGS="-I$HOME/opt/$SSL/include"
+export LDFLAGS="-L$HOME/opt/$SSL/lib"
+
+if [ ! -d "$HOME/opt/$EVENT" ]; then
+	wget "$EVENTURL" || exit 1
+	tar -xzvf "$EVENT-stable.tar.gz" || exit 1
+	cd "$EVENT-stable" || exit 1
+	if [ -n "$EVENTPATCH" ]; then
+		patch -p0 < ../$EVENTPATCH || exit 1
+	fi
+	./configure --prefix="$HOME/opt/$EVENT" || exit 1
+	make && make install || { rm -rf "$HOME/opt/$EVENT"; exit 1; }
+	cd ..
+fi
+
